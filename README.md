41188a6949c6910b2a30171527c2089e5527a1a3
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-11-28 01:19:05 -0500

    I don't know at this point

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+[Lighthouse](Lighthouse.png)
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function Footer() {
+  return (
+    <footer>
+      &copy; 2023 Be Kind
+    </footer>
+  )
+}
+
+export default Footer;
